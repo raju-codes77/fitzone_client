e251e12af4d81cc28a32648eb1bfcf5983624247
Yajuddin Raju <yajuddinraju1@gmail.com>
2026-06-29 08:49:23 -0400

    google authentication

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'images.unsplash.com',
         pathname: '/**',
-      }
+      },{
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
     ],
   },
 };
