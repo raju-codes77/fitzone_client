bcf71c0ee9389de6b85a2f01c6eb8b5d1db96528
Yajuddin Raju <yajuddinraju1@gmail.com>
2026-09-19 13:52:19 -0400

    Fix duplicate authClient configuration

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -2,9 +2,8 @@ import { createAuthClient } from "better-auth/react";
 import { jwtClient } from "better-auth/client/plugins";
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: process.env.BETTER_AUTH_URL,
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL_AUTH || process.env.BETTER_AUTH_URL,
     plugins: [jwtClient()]
-
 })
 
-export const { signIn, signUp, useSession } = createAuthClient()
+export const { signIn, signUp, useSession } = authClient;
